db47bb2dcbebf9d4ec0e29b7b1992da9bc85511e
realmadrid2 <tohran1@etf.unsa.ba>
2026-02-12 19:37:40 +0100

    Initial commit - Student System project setup

---
+++ src/main/java/com/tariketf/student_system_information/StudentSystemInformationApplication.java
@@ -0,0 +1,13 @@
+package com.tariketf.student_system_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSystemInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSystemInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tariketf/student_system_information/StudentSystemInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tariketf.student_system_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSystemInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
